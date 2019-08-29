f95ccb37a2655a21d518ba7ad806348843f7c042
BureikuT <bureikut@hotmail.com>
2019-08-29 13:01:13 -0600

    cleaned jsx by switching from inline styling to a stylesheet object

--- App.js
+++ App.js
@@ -1,23 +1,33 @@
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
-
-  const [outputText, setOutputText] = useState('BLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAGH')
+  const [outputText, setOutputText] = useState(
+    "BLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAGH"
+  );
 
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title="PUSH ME" onPress={()=>setOutputText('The Text Changed')} />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.input}
+        />
+        <Button title="Add" />
+      </View>
+
+      <View></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ffff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  }
 });
